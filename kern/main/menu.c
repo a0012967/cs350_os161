a0675b9c19b0b45a10f744162fd75b8d79cc64cd
Estelle Wan <erwanpangcheung@uwaterloo.ca>
2013-02-18 16:13:40 -0500

    Menu to support arguments passing for runprogram

--- kern/main/menu.c
+++ kern/main/menu.c
@@ -57,19 +57,19 @@ cmd_progthread(void *ptr, unsigned long nargs)
 	char **args = ptr;
 	char progname[128];
 	int result;
-
+        
 	assert(nargs >= 1);
 
-	if (nargs > 2) {
-		kprintf("Warning: argument passing from menu not supported\n");
-	}
+	//if (nargs > 2) {
+	//	kprintf("Warning: argument passing from menu not supported\n");
+	//}
 
 	/* Hope we fit. */
 	assert(strlen(args[0]) < sizeof(progname));
 
 	strcpy(progname, args[0]);
-
-	result = runprogram(progname);
+        
+	result = runprogram(progname, args+1, nargs);
 	if (result) {
 		kprintf("Running program %s failed: %s\n", args[0],
 			strerror(result));
