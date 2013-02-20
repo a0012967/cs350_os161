62171fd79e0de8325160e8d26614bba88bb8d3f9
Estelle Wan <erwanpangcheung@uwaterloo.ca>
2013-02-20 11:58:54 -0500

    Runprogram now passing arguments but with TLB errors

--- kern/userprog/runprogram.c
+++ kern/userprog/runprogram.c
@@ -22,7 +22,7 @@
  * Calls vfs_open on progname and thus may destroy it.
  */
 int
-runprogram(char *progname)
+runprogram(char *progname, char** argv, int argc)
 {
 	struct vnode *v;
 	vaddr_t entrypoint, stackptr;
@@ -64,9 +64,49 @@ runprogram(char *progname)
 		/* thread_exit destroys curthread->t_vmspace */
 		return result;
 	}
-
+        
+        unsigned int i;
+        unsigned int j;
+        
+        for (i = 0; i < argc; i++) // copyout the array values
+        {
+            j = argc-(i+1);
+            
+            unsigned int len = strlen(argv[j]); // length of actual string
+            unsigned int modArg = (len+1)%4;    // mod 4
+            unsigned int totalLen = len+1+(4-modArg); // total correct alignment length(multiple of 4)
+            char argPadding[totalLen]; // array for storing arguments with padding
+            
+            strcpy(argPadding, argv[j]);
+            
+            if (modArg != 0) // we need to pad
+            {
+                // padding with '\0'
+                unsigned int k;
+                for (k = len+1; k < totalLen; k++)
+                    argPadding[k] = '\0';
+            }
+            
+            stackptr = stackptr-totalLen;
+            argv[j] = stackptr; // fill argv with actual user space ptr
+            
+            copyout(&argPadding, stackptr, totalLen);
+            
+        }
+        
+        for (i = 0; i <= argc; i++) // copyout the array addresses
+        {
+            j = argc-i;
+            stackptr = stackptr-4;
+            
+            if (i == 0)
+               copyout(NULL, stackptr, 4);
+            else
+               copyout(&argv[j], stackptr, 4);
+        }
+        
 	/* Warp to user mode. */
-	md_usermode(0 /*argc*/, NULL /*userspace addr of argv*/,
+	md_usermode(argc /*argc*/, stackptr /*userspace addr of argv*/,
 		    stackptr, entrypoint);
 	
 	/* md_usermode does not return */
