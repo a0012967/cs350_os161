c72299b90464a6479a1c91e450cf31075045ba5e
cecabusa <cecabusa@uwaterloo.ca>
2013-02-25 19:23:58 -0500

    trying to pull

--- kern/syscalls/write.c
+++ kern/syscalls/write.c
@@ -15,7 +15,7 @@
 //Must ensure that we only allow one thread to do any of the syscalls
 
 volatile struct lock *syslock = NULL;
-
+volatile int offset = 0;
 
 static void init(){
 
@@ -32,7 +32,7 @@ write(int fd, const void *buf, size_t nbytes){
     bot = (vaddr_t) buf;
     top = bot + nbytes -1;
     
-    if(fd < 0){
+    if(fd < 0 || !buf){
         
         return EBADF;
         
@@ -47,34 +47,30 @@ write(int fd, const void *buf, size_t nbytes){
    
     
     struct uio u_write; //used to hold data to write
-    mk_kuio(&u_write,buf,nbytes,bot,UIO_WRITE);
+    mk_kuio(&u_write,buf,nbytes,0,UIO_WRITE);
   
     struct iovec iovec_write;
     struct vnode *vn;
     char *console = NULL;
     console = kstrdup("con:");
-    //kprintf("lock about to acquire\n");
+
     lock_acquire(syslock);
-   // kprintf("lock acquire success\n");
+   
     int result = vfs_open(console,O_WRONLY,&vn);
     
     int result2 = VOP_WRITE(vn,&u_write);
-   //vfs_close(vn);
+    kprintf("write result2 is: %d\n",result2);
         lock_release(syslock);    
-       // kprintf("released lock\n");
+       
     kfree(console);    
-    
-    
-  // kprintf("result2 is %d\n",result2);
+ 
     if(result2){
         
-        return result2; //should also return EIO and ENOSPC
+        return -1; //should also return EIO and ENOSPC
     }
 
 
 
-    
-    
  
     
    return nbytes - u_write.uio_resid; //returns how many were written
@@ -85,57 +81,49 @@ write(int fd, const void *buf, size_t nbytes){
 
 
 
-int read(int filehandle, void *buf, size_t size){
-
-
+int read(int fd, void *buf, size_t nbytes){
+    
+    
     vaddr_t bot,top;
     bot = (vaddr_t) buf;
-    top = bot + size -1;
+    top = bot + nbytes -1;
     
-    if(filehandle < 0){
+    if(fd < 0 || !buf){
         
         return EBADF;
         
     }
-    if(top < bot){
+    if(top < bot || !buf){
         
         return EFAULT;
     }
-
-
-
-   init();
-   
-    struct uio u_read;
-    mk_kuio(&u_read,buf,size,0,UIO_READ);//sets up for read
-  
-    //struct iovec iovec_read;
+    
+    init();
+    
+    
+    
+    struct uio u_read; //used to hold data to write
+    mk_kuio(&u_read,buf,nbytes,offset,UIO_READ);
+    
+    struct iovec iovec_write;
     struct vnode *vn;
     char *console = NULL;
     console = kstrdup("con:");
-    //kprintf("lock about to acquire\n");
-  //  lock_acquire(syslock);
-   // kprintf("lock acquire success\n");
-   kprintf("read1\n");
+    
+    lock_acquire(syslock);
+    
     int result = vfs_open(console,O_RDONLY,&vn);
-    kprintf("read1end\n");
+    
     int result2 = VOP_READ(vn,&u_read);
-    kprintf("read2\n");
-    //    lock_release(syslock);    
-       // kprintf("released lock\n");
+    
+    lock_release(syslock);    
+    
     kfree(console);    
     
-    
-
     if(result2){
-        kprintf("ifststament\n");
+        
         return result2; //should also return EIO and ENOSPC
     }
-
-kprintf("result2 cond failed, result2:%d \n",result2);
-
-//kprintf("return: %d\n",size - u_read.uio_resid);
-
-   return size - u_read.uio_resid; //returns how many were written
-  //return 0;
+    
+    return nbytes - u_read.uio_resid; //returns how many were written
 }
