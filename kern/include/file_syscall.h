a1c5153496c2054abeaf13004be878280d637955
cecabusa <cecabusa@uwaterloo.ca>
2013-03-09 15:54:36 -0500

    added file_syscall.h

---
+++ kern/include/file_syscall.h
@@ -0,0 +1,15 @@
+#ifndef _file_syscall_H_
+#define _file_syscall_H_
+
+#include "opt-A2.h"
+
+
+#if OPT_A2
+int sys_open(userptr_t filename, int flags, int mode, int *retval);
+int sys_close(int fd);
+int write(int fd, const void *buf, size_t nbytes);
+int read(int fd, void *buf, size_t nbytes,int *err);
+void _exit(int exitcode,int *retval);
+#endif /* _OPT_A2_ */
+
+#endif //define
